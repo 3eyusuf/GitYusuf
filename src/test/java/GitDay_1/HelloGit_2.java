bf7e4d9eedcd3db3fa13563423d525db803fc3ec
3eyusuf <3eyusuf@gmail.com>
2022-03-17 20:10:27 +0100

    Test 2

--- src/test/java/GitDay_1/HelloGit_2.java
+++ src/test/java/GitDay_1/HelloGit_2.java
@@ -6,4 +6,24 @@ public class HelloGit_2 {
 
     // Tekrar yenilik
 
+    /* ->git init -- ilk kez projemize GIT i tanimlamak icin kullanilir, tek seferlik calistirilir.
+
+.gitignore   -->  ozel bir file dir. git in track sistemine dahil etmek istemediginiz dosya ve/veya  klasorler bu file icin de satir satir tutulur.
+
+Sadece LOCAL de calisma yapacak iseniz alttaki mail ve isim degerleri eklenmeli!!!
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+->git add dosyaIsmi.uzantisi  -- untrackt file lara kancayi takmak icin / tracked yapmak icin kullanilir
+
+->git add . -- butun untracked dosylari EKLEMEK icin / yani tracked yapmak icin kullanilir!
+
+->git commit -m"commit mesaji yaz"  -- eklenen ve tracked olan dosyalari KAYDETMEK ICIN (Snapshot!!)
+                                           peki nereye -> Local git directory e!
+
+
+->git status -- untracked , tracked ve modifed dosyalari bize getirir gosterir
+
+->git log -- commit gecmisini gosterir */
+
 }
